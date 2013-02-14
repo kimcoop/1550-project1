d3cb87b1d5ff1a4863c5acb2d4ea0a55d47eded7
Kim Cooperrider <kim.cooperrider@gmail.com>
2013-02-13 23:50:07 -0500

    removal old files

--- LinkedList.h
+++
@@ -1,7 +0,0 @@
-struct item 	*CreateItem(struct item *);
-struct item 	*InsertItem(struct item *, char *);
-struct item 	*DeleteItem(struct item *, char *);
-char		*GetFirstItem(struct item *);
-char		*GetLastItem(struct item *);
-int		IsMemberOfList(struct item *, char *);
-void		PrintItem(struct item *);
--- OrderedLinkedList.h
+++
@@ -1,7 +0,0 @@
-struct item 	*OrderedCreateItem(struct item *);
-struct item 	*OrderedInsertItem(struct item *, char *);
-struct item 	*OrderedDeleteItem(struct item *, char *);
-char		*GetSmallestItem(struct item *);
-char		*GetLargestItem(struct item *);
-int		IsMemberOfOrderedList(struct item *, char *);
-void		OrderedPrintItem(struct item *);
